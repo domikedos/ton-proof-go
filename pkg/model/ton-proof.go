87d7039aa7014a2d50dd6bb8ad45d9faad3eaf58
Pasha <pashalevchenko703@gmail.com>
2024-12-02 19:32:22 +0600

    feat: add ton proof model

---
+++ pkg/model/ton-proof.go
@@ -0,0 +1,21 @@
+package model
+
+type Domain struct {
+	LengthBytes uint32 `json:"lengthBytes"`
+	Value       string `json:"value"`
+}
+
+type Proof struct {
+	Timestamp int64  `json:"timestamp"`
+	Domain    Domain `json:"domain"`
+	Signature string `json:"signature"`
+	Payload   string `json:"payload"`
+	StateInit string `json:"state_init"`
+}
+
+type TonProof struct {
+	Address   string `json:"address"`
+	Network   string `json:"network"`
+	PublicKey string `json:"public_key"`
+	Proof     Proof  `json:"proof"`
+}
